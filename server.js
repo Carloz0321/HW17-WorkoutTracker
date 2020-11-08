053e368f2d096de61340e3542e36e5a82f8c72b5
Carlos Aguilar <carloz0321@gmail.com>
2020-11-07 18:33:11 -0800

    Make it better

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const PORT = express.env.PORT || 3030;
+const app = express();
+
+app.use(morgan("dev"));
+
+
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+
+mongoose.connect(MONGODB_URI, {
+    userNewUrlPaser: true,
+    userFindAndModify: false
+});
+
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
